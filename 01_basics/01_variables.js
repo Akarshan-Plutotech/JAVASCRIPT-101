57a06153aa7b3fb85a10e19fd0b02d8032e767c3
Akarshan Pandey <akarshanpandeyworks@gmail.com>
2025-06-20 03:30:25 +0000

    Dataypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const account_ID = "986700"
+let account_email = "akarshan@gmail.com"
+var account_pass = "122112"
+account_city = "manglore"
+
+// const account_ID = "3455444" as there exists alredy a const hence this isnt allowed
+
+// below are the stuff which are allowed or can be chnaged ever after changes are made
+
+account_email = "atul@google.com"
+account_pass = "26646"
+account_city = "mumbai" // lets see if it works w/o inverted commaas
+
+console.log(account_ID);
+
+console.log([account_ID, account_email, account_pass, account_city]);
+
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+//alert(3 + 3) // this won't show answer SOMEHOW, cus we using nodejs and not broweser
+
+// we ashould maintain proper formatiiing everytime, it should be presentable
+
+/* DATA TYPES */
+
+let name = "AKArshan"
+let age = 18
+let isLoggedIn = false
+
+//TYEPS
+
+// number => 2^53 ka limit hai
+// bigint => for big no.s
+// string => ""
+// boolean => true/false
+// null => standalone value
+// symbol => unique
+// undefined
+
+//object => importatnt
+
+console.log(typeof "AKArshan");
+console.log(typeof 18);
