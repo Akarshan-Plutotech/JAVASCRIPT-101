7288f6efe2b725658c66aea8b5e7b75b94bb7748
Akarshan Pandey <akarshanpandeyworks@gmail.com>
2025-06-22 06:33:07 +0000

    coversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,42 @@
+let result = 1
+//console.log(result);
+
+let result1 = "5check"
+//console.log(typeof result1);
+
+let result3 = "ram ram"
+// console.log(typeof result3);
+// console.log(result3);
+
+// CONVERSIONS:
+
+let bsicute = Number(result1)
+//console.log(typeof bsicute);
+//console.log(bsicute);
+
+let jorden = NaN
+//console.log(typeof jorden);
+//console.log(jorden);
+
+let pika = undefined
+//console.log(typeof pika);
+//console.log(pika);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// BOOLEAN STUDY
+
+let value = "44"
+let check = Boolean(value)
+
+console.log(check);
+console.log(typeof check);
+
+// "" = False
+//  22 = True
+// "22" = True
+// "ram" = True
+// ram = false/no answer
