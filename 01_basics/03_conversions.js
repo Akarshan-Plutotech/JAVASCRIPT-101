f7240d1889e34b658cfddd8751d68ba8f3e09c4d
Akarshan Pandey <akarshanpandeyworks@gmail.com>
2025-06-25 07:12:21 +0000

    coversionoperations kiya

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -32,11 +32,38 @@ let pika = undefined
 let value = "44"
 let check = Boolean(value)
 
-console.log(check);
-console.log(typeof check);
+// console.log(check);
+// console.log(typeof check);
 
 // "" = False
 //  22 = True
 // "22" = True
 // "ram" = True
 // ram = false/no answer
+
+
+//************OPERATIONS***********
+
+let newvalue = 22
+let negvalue = -newvalue
+// console.log(negvalue);
+
+let str1 = "Queer Mahajan"
+let str2 = " is actually trans SC/ST Queer"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// PREFIX and POSTFIX ++
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
