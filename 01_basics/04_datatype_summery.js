5c31b78e56fe1e5355ed74ffdf1d89a7fa0348c1
Akarshan Pandey <akarshanpandeyworks@gmail.com>
2025-06-26 12:12:33 +0000

    new

---
+++ 01_basics/04_datatype_summery.js
@@ -0,0 +1,18 @@
+// PRIMITIVE DATA TYPES
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Undefined
+// 5. Null
+// 6. Symbol (ES6)
+// 7. BigInt (ES11)
+
+// REFRENCE DATA TYPES
+// 1. Object - MASTER THIS BRUH
+// 2. Array
+// 3. Function
+
+const score = 100; // Number
+const scoreValue = 100.5; // Number with decimal
+
+const isLoggedIn = true; // Boolean
