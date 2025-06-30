aeb72b7f6d3ea811c4b80951f29cd6574618df33
Akarshan Pandey <akarshanpandeyworks@gmail.com>
2025-06-30 18:49:01 +0000

    maths

---
+++ 01_basics/06_numbs_maths.js
@@ -0,0 +1,36 @@
+const Score = 100;
+// console.log(`Your score is ${Score}`);
+
+const balance = 500;
+// console.log(`Your balance is ${balance}`);
+
+const newScore = Number('100');
+// console.log(newScore);
+
+const newBalance = new Number(500);
+// console.log(newBalance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toString(2)); // Binary representation
+// console.log(balance.toFixed(2)); // Hexadecimal representation
+
+const newNumb = 99.596788;
+// console.log(newNumb.toPrecision(3)); // 99.6
+// console.log(newNumb.toPrecision(5)); // 99.597
+
+const dollar = 5000000000;
+// console.log(dollar.toLocaleString()); // US version
+// console.log(dollar.toLocaleString('en-IN')); // Indian version
+// console.log(dollar.toLocaleString('de-DE')); // German version
+
+
+// +++++++++++++++++ Maths +++++++++++++++++
+
+const num1  = 100;
+
+//
+
+
+console.log(num1); // 101
+console.log(++num1); // Pre-increment, now 102
+console.log(num1--); // Post-decrement, now 102
