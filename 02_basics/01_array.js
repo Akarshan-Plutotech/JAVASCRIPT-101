ab57aabf653859d9148545cbdd12c3762f5df237
Akarshan Pandey <akarshanpandeyworks@gmail.com>
2025-07-08 19:06:39 +0000

    just splicing hehehe

---
+++ 02_basics/01_array.js
@@ -0,0 +1,51 @@
+// ARRAY - an object, where we can put multiple iteams in single variable (myArr here!)
+
+// const myArr = [0, 1, 2, 3, 4, 5]
+// // console.log(myArr);
+
+// const myHeros = ["Superman", "Spiderman"]
+
+// const myArr2 = new Array(1, 2, 3, 4)
+// // console.log(myArr2[0]);
+
+
+// ARRY METHOS
+
+// myArr.push(6)
+// myArr.push(7) // we had only 6 values 0 to 5 as we added push 7, 7 also got added to o/p
+
+// console.log(myArr);
+
+// myArr.unshift(0)
+// myArr.unshift(9)
+myArr.unshift()
+myArr.shift() //initial vlaue ie 0 vo gayab hogaya
+
+// console.log(myArr); //this added two values in the front of the values like 0 and 
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf(3));
+// console.log(myArr);
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// ++++++++++ SLICE , SPLICE +++++++++++++
+
+// console.log("A ", myArr);
+// const myn1 = myArr.slice(1, 3);
+
+// console.log(myn1);
+// console.log("B ", myArr);
+
+// const myn2 = myArr.splice(1, 3);
+// console.log("c ", myArr);
+// console.log(myn2);
+
+
+
+
